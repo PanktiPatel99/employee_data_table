eb8d91c3c9420b9cce0c0b5970496ebbcd8bf402
PanktiPatel99 <panktip.mobio@gmail.com>
2022-09-01 14:46:52 +0530

    Employee data table

--- src/Components/Employee.js
+++ src/Components/Employee.js
@@ -1,11 +1,31 @@
 import React from "react";
 import EmployeeData from "./EmployeeData";
+import "./Employee.css";
 
 const Employee = () => {
   return (
-    <>
-      <div className="employee">Employees</div>
-    </>
+    <div className="employee">
+      <table className="emp_table" style={{ border: "2px solid black" }}>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>City</th>
+            <th>Email</th>
+            <th>PhoneNumber</th>
+          </tr>
+        </thead>
+        <tbody>
+          {EmployeeData.map((emp) => (
+            <tr key={emp.Id} data={emp}>
+              <td>{emp.Name}</td>
+              <td>{emp.City}</td>
+              <td>{emp.Email}</td>
+              <td>{emp.PhoneNumber}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 };
 export default Employee;
