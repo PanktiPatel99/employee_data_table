52c86ea2d8cf9c061e81d74e0cb97c9152a37ff9
PanktiPatel99 <panktip.mobio@gmail.com>
2022-09-01 11:04:33 +0530

    create Employee data list

---
+++ src/Components/Employee.js
@@ -0,0 +1,11 @@
+import React from "react";
+import EmployeeData from "./EmployeeData";
+
+const Employee = () => {
+  return (
+    <>
+      <div className="employee">Employees</div>
+    </>
+  );
+};
+export default Employee;
---
+++ src/Components/EmployeeData.js
@@ -0,0 +1,46 @@
+const EmployeeData = [
+  {
+    Id: 1,
+    Name: "Jiaan",
+    City: "Ahmedabad",
+    Email: "jiaan@gmail.com",
+    PhoneNumber: 1234567845,
+  },
+  {
+    Id: 2,
+    Name: "Abhinav",
+    City: " Vadodra",
+    Email: "abhi@gmail.com",
+    PhoneNumber: 3427659758,
+  },
+  {
+    Id: 3,
+    Name: "Rahul",
+    City: "Surat",
+    Email: "rahul@gmail.com",
+    PhoneNumber: 3087597452,
+  },
+  {
+    Id: 4,
+    Name: "Dax",
+    City: "Ahmedabad",
+    Email: "dax@gmail.com",
+    PhoneNumber: 4720978659,
+  },
+  {
+    Id: 5,
+    Name: "Siaan",
+    City: "Surat",
+    Email: "siaan@gmail.com",
+    PhoneNumber: 9342561970,
+  },
+  {
+    Id: 6,
+    Name: "Pankti",
+    City: "Vadodra",
+    Email: "pankti@gmail.com",
+    PhoneNumber: 7630286340,
+  },
+];
+
+export default EmployeeData;
