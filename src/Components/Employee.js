6cb7836fe8400d8d0b3762b726a40e4988f0b35b
PanktiPatel99 <panktip.mobio@gmail.com>
2022-09-02 11:28:07 +0530

    add new contact row in table using form

--- src/Components/Employee.js
+++ src/Components/Employee.js
@@ -1,9 +1,48 @@
 import React, { useState } from "react";
+import { nanoid } from "nanoid";
 import EmployeeData from "./EmployeeData";
 import "./Employee.css";
 
 const Employee = () => {
   const [contact, setContact] = useState(EmployeeData);
+  const [addFormData, setAddFormData] = useState({
+    Name: "",
+    City: "",
+    Email: "",
+    PhoneNumber: "",
+  });
+
+  const addFormChangeHandle = (e) => {
+    e.preventDefault();
+    // console.log(e);
+    const fieldName = e.target.getAttribute("name");
+    const fieldValue = e.target.value;
+
+    const newFormData = { ...addFormData };
+    newFormData[fieldName] = fieldValue;
+    // console.log(newFormData);
+
+    setAddFormData(newFormData);
+  };
+
+  const addFormSubmitHandle = (e) => {
+    e.preventDefault();
+
+    const newContact = {
+      Id: nanoid(),
+      Name: addFormData.Name,
+      City: addFormData.City,
+      Email: addFormData.Email,
+      PhoneNumber: addFormData.PhoneNumber,
+    };
+    console.log(newContact);
+
+    const newContacts = [...contact, newContact];
+    console.log(newContacts);
+
+    setContact(newContacts);
+  };
+
   return (
     <div className="employee">
       <table style={{ border: "2px solid black" }}>
@@ -26,6 +65,39 @@ const Employee = () => {
           ))}
         </tbody>
       </table>
+
+      <h2>Add a New Employee</h2>
+      <form onSubmit={addFormSubmitHandle}>
+        <input
+          type="text"
+          name="Name"
+          placeholder="Enter a name"
+          required
+          onChange={addFormChangeHandle}
+        />
+        <input
+          type="text"
+          name="City"
+          placeholder="Enter a city"
+          required
+          onChange={addFormChangeHandle}
+        />
+        <input
+          type="email"
+          name="Email"
+          placeholder="Enter an email"
+          required
+          onChange={addFormChangeHandle}
+        />
+        <input
+          type="number"
+          name="PhoneNumber"
+          placeholder="Enter a phone number"
+          required
+          onChange={addFormChangeHandle}
+        />
+        <button type="submit">Add</button>
+      </form>
     </div>
   );
 };
